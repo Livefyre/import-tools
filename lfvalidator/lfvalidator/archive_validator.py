b4672c2f0b7da225c6312ddfb8859a3c7c5329a4
Michael Liao <mliao@livefyre.com>
2015-04-13 17:57:37 -0700

    add archive file validator

---
+++ lfvalidator/lfvalidator/archive_validator.py
@@ -0,0 +1,14 @@
+from validator import validate
+
+def validate_archive(infile, outfile='validator_results.txt'):
+	validate(infile, outfile, is_archive=True)
+
+def main():
+    args = sys.argv[1:]
+    if len(args) not in (1,2,3):
+        print 'Usage: python archive_validator.py [input file] [~optional schema file] [~optional output file]'
+        sys.exit(0)
+    validate_archive(*args)
+
+if __name__ == '__main__':
+    main()
